d0031d0d0d7db07d1621771dd4b3ccfe61d87f04
Jakub Kuliński <s33988@pjwstk.edu.pl>
2026-03-23 19:14:31 +0100

    Add base equipment model

---
+++ Equipment.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2;
+
+public abstract class Equipment
+{
+    
+    public int Id { get; }
+    public string Name { get; set; }
+    public EquipmentStatus Status {get; set;}
+    public static int IdCounter = 1;
+
+    public Equipment(string name)
+    {
+        Name = name;
+        Id = IdCounter++;
+        Status = EquipmentStatus.Available;
+    }
+}
