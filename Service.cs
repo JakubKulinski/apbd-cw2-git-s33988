567eb3c7bc9f4eff278101fd6db1a4beaaa86488
Jakub Kuliński <s33988@pjwstk.edu.pl>
2026-03-23 19:58:18 +0100

    Add RentEquipment method

--- Service.cs
+++ Service.cs
@@ -40,4 +40,33 @@ public class Service
             }
         }
     }
+    
+    public void RentEquipment(int userId, int equipmentId, int days)
+    {
+        var user = Users.FirstOrDefault(u => u.Id == userId);
+        var equipment = EquipmentList.FirstOrDefault(e => e.Id == equipmentId);
+
+        if (equipment == null || user == null)
+        {
+            Console.WriteLine("User or  equipment not found");
+            return;
+        }
+
+        if (!equipment.Status.Equals(EquipmentStatus.Available))
+        {
+            Console.WriteLine("Equipment is not available");
+            return;
+        }
+
+        var currentRenting = RentingList.Count(r => r.User.Id == user.Id);
+        if (currentRenting >= 3)
+        {
+            Console.WriteLine("User exceeded rental limit.");
+            return;
+        }
+        
+        var renting = new Renting(user, equipment, days);
+        RentingList.Add(renting);
+        equipment.Status = EquipmentStatus.Unavailable;
+    }
 }
