5b02d7209c89b4ef01ef25a276a0ec0fe49a7b05
Jakub Kuliński <s33988@pjwstk.edu.pl>
2026-03-23 19:48:18 +0100

    Add Service class

---
+++ Service.cs
@@ -0,0 +1,15 @@
+﻿namespace apbd_cw2_git_s33988;
+
+public class Service
+{
+    public List<User> Users { get; set; }
+    public List<Equipment> EquipmentList { get; set; }
+    public List<Renting> RentingList { get; set; }
+
+    public Service()
+    {
+        Users = new List<User>();
+        EquipmentList = new List<Equipment>();
+        RentingList = new List<Renting>();
+    }
+}
